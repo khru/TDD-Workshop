3b8df90a9b2fd22a70b78080117e849385684a52
PabloTejGar <pablo@MBP-de-Pablo.home>
2023-03-10 16:35:19 +0100

    red:test_given_a_3_then_we_get_a_fizz

--- python/tests/test_fizz_buzz.py
+++ python/tests/test_fizz_buzz.py
@@ -8,11 +8,16 @@ class TestFizzBuzz:
     @pytest.mark.parametrize("number, expected_result",
                              [(1, "1"),
                               (2, "2"),
-                              (4, "4")
+                              (4, "4"),
+                              (7, "7")
                               ])
     def test_given_a_number_than_is_not_divisible_by_3_or_5_we_get_the_same_number_as_a_str(
             self,
             number: int,
             expected_result: str,
     ):
-        assert (fizz_buzz(number), expected_result)
+        assert fizz_buzz(number) == expected_result
+
+    @pytest.mark.current
+    def test_given_a_3_then_we_get_a_fizz(self):
+        assert fizz_buzz(3) == "Fizz"
