e88570e26fa75dfea15ddac86cac067763767b0d
PabloTejGar <pablo@MBP-de-Pablo.home>
2023-03-10 16:13:03 +0100

    red: test_given_a_2_we_expect_a_2

--- python/tests/test_fizz_buzz.py
+++ python/tests/test_fizz_buzz.py
@@ -7,3 +7,8 @@ class TestFizzBuzz:
             self,
     ):
         assert fizz_buzz(1) == "1"
+
+    def test_given_a_2_we_expect_a_2(
+            self,
+    ):
+        assert fizz_buzz(2) == "2"
