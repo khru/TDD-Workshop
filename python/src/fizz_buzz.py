5cd5c90e5312ff382f6e79457bb4273b44ceec57
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-10 17:25:06 +0100

    refactor: create dict with business logic

--- python/src/fizz_buzz.py
+++ python/src/fizz_buzz.py
@@ -1,22 +1,14 @@
 def fizz_buzz(
         input: int
 ) -> str:
-    if is_multiple_of_fifteen(input):
-        return "FizzBuzz"
-    if is_multiple_of_three(input):
-        return "Fizz"
-    if is_multiple_of_five(input):
-        return "Buzz"
+    fizzbuzz: dict = {
+        15: "FizzBuzz",
+        3: "Fizz",
+        5: "Buzz",
+    }
+    for key in fizzbuzz:
+        if input % key == 0:
+            return fizzbuzz[key]
+
     return str(input)
 
-
-def is_multiple_of_three(number: int) -> bool:
-    return number % 3 == 0
-
-
-def is_multiple_of_five(number: int) -> bool:
-    return number % 5 == 0
-
-
-def is_multiple_of_fifteen(number: int) -> bool:
-    return number % 15 == 0
