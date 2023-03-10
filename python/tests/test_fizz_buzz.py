03f1b3d8338feda5820cac6f30e3f8ab9c3c1f76
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-10 16:58:07 +0100

    red: test_given_a_20_that_we_expect_to_return_buzz

--- python/tests/test_fizz_buzz.py
+++ python/tests/test_fizz_buzz.py
@@ -37,3 +37,5 @@ class TestFizzBuzz:
     def test_given_a_10_that_we_expect_to_return_buzz(self):
         assert fizz_buzz(10) == "Buzz"
 
+    def test_given_a_20_that_we_expect_to_return_buzz(self):
+        assert fizz_buzz(20) == "Buzz"
