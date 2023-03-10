55d57a9c396fef4a850baa1eb5206fbf2fde0909
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-10 17:11:13 +0100

    refactor: extract to method is multple of 5 and 3

--- python/src/fizz_buzz.py
+++ python/src/fizz_buzz.py
@@ -1,11 +1,18 @@
-
 def fizz_buzz(
-    input: int
+        input: int
 ) -> str:
     if input == 15:
         return "FizzBuzz"
-    if input % 3 == 0:
+    if is_multiple_of_three(input):
         return "Fizz"
-    if input % 5 == 0:
+    if is_multiple_of_five(input):
         return "Buzz"
     return str(input)
+
+
+def is_multiple_of_three(number: int) -> bool:
+    return number % 3 == 0
+
+
+def is_multiple_of_five(number: int) -> bool:
+    return number % 5 == 0
