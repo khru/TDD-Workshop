08acb24082e05db4f898178f0a056be561569394
PabloTejGar <pablo@MBP-de-Pablo.home>
2023-03-10 16:29:06 +0100

    refactor:parametrize tests

--- python/tests/test_fizz_buzz.py
+++ python/tests/test_fizz_buzz.py
@@ -1,19 +1,18 @@
+import pytest
+
 from src.fizz_buzz import fizz_buzz
 
 
 class TestFizzBuzz:
 
-    def test_given_a_1_we_expect_a_1(
+    @pytest.mark.parametrize("number, expected_result",
+                             [(1, "1"),
+                              (2, "2"),
+                              (4, "4")
+                              ])
+    def test_given_a_number_than_is_not_divisible_by_3_or_5_we_get_the_same_number_as_a_str(
             self,
+            number: int,
+            expected_result: str,
     ):
-        assert fizz_buzz(1) == "1"
-
-    def test_given_a_2_we_expect_a_2(
-            self,
-    ):
-        assert fizz_buzz(2) == "2"
-
-    def test_given_a_4_we_expect_a_4(
-            self,
-    ):
-        assert fizz_buzz(4) == "4"
+        assert (fizz_buzz(number), expected_result)
